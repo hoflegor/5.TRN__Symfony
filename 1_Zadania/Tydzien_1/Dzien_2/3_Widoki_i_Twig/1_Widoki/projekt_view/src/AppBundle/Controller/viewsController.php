9603130a33093bf90b86fc3b045a09cab1dbc8c8
thoffmann <thoffmann@unity-t.pl>
2019-02-12 18:01:29 +0100

    day 2

--- 1_Zadania/Tydzien_1/Dzien_2/3_Widoki_i_Twig/1_Widoki/projekt_view/src/AppBundle/Controller/viewsController.php
+++ 1_Zadania/Tydzien_1/Dzien_2/3_Widoki_i_Twig/1_Widoki/projekt_view/src/AppBundle/Controller/viewsController.php
@@ -5,7 +5,8 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Article;
 
 class viewsController extends Controller
 {
@@ -62,17 +63,19 @@ class viewsController extends Controller
 	 * @Route("/repeatSentence/{n}/{sentc}", defaults={"sentc"=null})
 	 * @Template("AppBundle:views:view_ex_b3.html.twig")
 	 */
-	public function repeatSentenceAction($n, $sentc){
+	public function repeatSentenceAction($n, $sentc)
+	{
 
-		return ['n' => $n, 'sentc'=>$sentc];
+		return ['n' => $n, 'sentc' => $sentc];
 
 	}
 
 	/**
-	*@Route("/createRandoms/{start}/{end}/{n}")
+	 * @Route("/createRandoms/{start}/{end}/{n}")
 	 * @Template("AppBundle:views:view_ex_b4.html.twig")
-	*/
-	public function createRandomsAction($start, $end, $n){
+	 */
+	public function createRandomsAction($start, $end, $n)
+	{
 
 		$randArr = [];
 
@@ -81,14 +84,31 @@ class viewsController extends Controller
 			array_push($randArr, rand($start, $end));
 		}
 
-		return ['arr'=>$randArr, 'n'=>$n];
+		return ['arr' => $randArr, 'n' => $n];
 
 	}
 
 
+	/**
+	 * @Route("/showAllArticles", name="allArticles")
+	 */
+	public function showAllArticlesAction()
+	{
+		$allArticles = Article::GetAllArticles();
 
+		return $this->render('AppBundle:views:showAllArticles.html.twig', array('allArticles' => $allArticles));
+	}
 
+	/**
+	 * @Route("/showArticle/{id}", name="showArticle")
+	 * @Template("AppBundle:views:showArticle.html.twig")
+	 */
+	public function showArticleAction($id)
+	{
+		$article = Article::GetArticleById($id);
 
+		return  array("article" => $article);
+	}
 
 
 }
